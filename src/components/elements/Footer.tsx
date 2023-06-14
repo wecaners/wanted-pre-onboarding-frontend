3bb1a6638d422ca014f76acfccbde56b185575e0
woody <90304025+wecaners@users.noreply.github.com>
2023-06-14 17:32:49 +0900

    Design: 기본 레이아웃 설정 (#3)

---
+++ src/components/elements/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className='flex items-center justify-center h-32 bg-blue-500 bg-opacity-90'>
+        <p className='text-sm text-white'>© Woody - pre-onboarding mission</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/elements/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <nav>
+      <div className='flex items-center justify-end h-12 bg-blue-500 bg-opacity-90'>
+        <div className='space-x-2'>
+          <button className='p-2 text-white bg-blue-500 rounded-full'>
+            회원가입
+          </button>
+          <button className='p-2 text-white bg-blue-500 rounded-full'>
+            로그인
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
