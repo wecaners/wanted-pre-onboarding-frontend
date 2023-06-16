bf0d057d3a7981ba04f1046084bbdbfe01e27bc1
woody <90304025+wecaners@users.noreply.github.com>
2023-06-16 17:11:34 +0900

    Feat: getTodo 에러메세지 수정 (#10)

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAccessToken } from '../../utils/localStorage';
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+  function handleClick() {
+    if (getAccessToken() !== null) {
+      navigate('/todo');
+    } else {
+      navigate('/signup');
+    }
+  }
   return (
     <div className='text-center'>
       <h1 className='text-3xl font-bold'>Wanted Frontend Pre Onboading</h1>
       <Link to='/signup'>
-        <button className='p-2 mt-5 border border-blue-500 rounded-2xl hover:bg-blue-500 hover:text-white'>
-          회원가입으로 시작하기
+        <button
+          className='p-2 mt-5 border border-blue-500 rounded-2xl hover:bg-blue-500 hover:text-white '
+          onClick={handleClick}
+        >
+          시작하기
         </button>
       </Link>
     </div>
