b15cf855e7a1ef798ce84f5ddd7ac022b0e8086b
woody <90304025+wecaners@users.noreply.github.com>
2023-06-16 16:49:27 +0900

    Feat: 로그아웃 기능 구현 (#9)

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -1,16 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAccessToken, removeAccessToken } from '../../utils/localStorage';
 
 export default function Header() {
+  const navigate = useNavigate();
+  function handleClick() {
+    removeAccessToken();
+    navigate('/');
+  }
   return (
     <nav>
-      <div className='flex items-center justify-end h-12 bg-blue-500 bg-opacity-90'>
-        <div className='space-x-2'>
+      <div className='flex justify-end h-12 bg-blue-500 bg-opacity-90'>
+        <div className='flex items-center space-x-2'>
           <Link to='/signup'>
             <button className='p-2 text-white'>회원가입</button>
           </Link>
-          <Link to='/signin'>
-            <button className='p-2 text-white'>로그인</button>
-          </Link>
+          {getAccessToken() !== null ? (
+            <>
+              <button className='p-2 text-white' onClick={handleClick}>
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to='/signin'>
+                <button className='p-2 text-white'>로그인</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
